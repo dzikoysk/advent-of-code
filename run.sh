fce79a2871214df34a387ea2e4d949b4d2f01111
dzikoysk <dzikoysk@dzikoysk.net>
2019-12-12 17:49:35 +0100

    Update Panda to indev-19-12-12

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Dinput=./2019/$1/input/$2.input "./sdk/panda-indev-19.11.19-all.jar" "./2019/$1/panda.hjson"
+java -jar -Dinput=./2019/$1/input/$2.input "./sdk/panda-indev-19.12.12-all.jar" "./2019/$1/panda.hjson"
